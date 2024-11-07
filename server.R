3ac1b1349c47be5645db852377a5a89e7da3ce86
D. Allan Drummond <dadrummond@gmail.com>
2024-11-06 22:07:27 -0600

    mostly working categories

--- server.R
+++ server.R
@@ -18,6 +18,23 @@ ps_dt <- read_tsv("scer_aggregation_psup_long.txt", comment='#') #,stringsAsFact
 
 theme_set(theme_minimal(base_size=14) %+replace% theme(legend.position="none"))
 
+splitstring = function(s) {
+  # Take string formatted as "ribosomal=(RPS1,RPS2); glycolytic=(GLK1,TDH3)" and split into a
+  # list(ribosomal=('RPS1','RPS2'), glycolytic=c('GLK1','TDH3'))
+  #if (T) {  s = "ribosomal=(rps1,RPS2); glycolytic=(GLK1,TDH3)" }
+  classes = str_split(s,';')[[1]]
+  x = sapply(classes, function(ss) {
+    res = str_split(ss,'=')[[1]]
+    label=gsub("[[:space:]]",'',res[1])
+    genes=toupper(str_split(gsub("\\((.*)\\)", "\\1",res[2]),',')[[1]])
+    g = list(g=genes)
+    names(g)[1] = label
+    print(g)
+    g
+  }, USE.NAMES = F)
+  x
+}
+
 scale_y_pSup <- function(name=NULL) {
     list(scale_y_continuous(name,expand=c(0.01,0),limits=c(0,1),
                        #breaks=seq(0,1,.25),labels=c("0","","0.5","","1"))
@@ -86,6 +103,80 @@ plotmygenes <- function(mygenes,data=ps_dt,
 
 }
 
+plotgenes_categories = function(gene_cat_list,data=ps_dt,
+                                tempexps=c("30C.rep2","37C.8min","42C.8min","46C.8min"),
+                                temps=c(30,37,42,46), tempbreaks=temps,
+                                timeexps=c("30C.rep1","46C.2min","46C.4min","46C.8min"),
+                                times=c(0,2,4,8), 
+                                errorbars=FALSE,linewidth=0.8,
+                                idType=c("gene","orf")) {
+  names(temps) = tempexps
+  names(times) = timeexps
+  
+  if (!is_list(gene_cat_list)) {
+    gene_cat_list = list("<unnamed>"=gene_cat_list)
+  }
+  
+  ps_dt_temp_all = bind_rows(lapply(names(gene_cat_list), function(cat) {
+    mygenes = gene_cat_list[[cat]]
+    ps_dt |> filter(.data[[idType]] %in% mygenes & experiment %in% tempexps) |> mutate(category=cat)
+  }))
+  
+  ps_dt_time_all = bind_rows(lapply(names(gene_cat_list), function(cat) {
+    mygenes = gene_cat_list[[cat]]
+    ps_dt |> filter(.data[[idType]] %in% mygenes & experiment %in% timeexps) |> mutate(category=cat)
+  }))
+  
+  # Summarize by category
+  ps_dt_temp = ps_dt_temp_all |> group_by(experiment, category) |> summarise(sd=sd(psup,na.rm=T), se=sd/sqrt(n()), psup=mean(psup), psup.lo=psup-sd, psup.hi=psup+sd, category=first(category))
+  ps_dt_time = ps_dt_time_all |> group_by(experiment, category) |> summarise(sd=sd(psup,na.rm=T), se=sd/sqrt(n()), psup=mean(psup), psup.lo=psup-sd, psup.hi=psup+sd, category=first(category))
+  
+  ps_dt_temp$temp = temps[ps_dt_temp$experiment ]
+  plot_temp = ggplot(data=ps_dt_temp,
+                     aes(x=.data[["temp"]],y=.data[["psup"]],ymin=.data[["psup.lo"]],ymax=.data[["psup.hi"]],
+                         colour=category,label=category)) +
+    geom_line(linewidth=linewidth) + 
+    geom_text_repel(size=4,data=ps_dt_temp |> filter(temp==max(temps)),
+                    aes(x=max(temps)+0.5,y=psup), xlim=c(46,52)) +
+    coord_cartesian(xlim=c(30,52)) +
+    #        scale_x_continuous(expression("Temperature "*(degree*C)*" of 8 min. treatment"),
+    scale_x_continuous("Temperature (\u00b0C) of 8 min. treatment",
+                       breaks=tempbreaks,labels=tempbreaks, expand=c(0,0)) +
+    scale_y_pSup("Proportion\nin\nsupernatant")
+  
+  ps_dt_time$time = times[ps_dt_time$experiment ]
+  
+  plot_time = ggplot(data=ps_dt_time,
+                     aes(x=.data[["time"]],y=.data[["psup"]],ymin=.data[["psup.lo"]],ymax=.data[["psup.hi"]],
+                         colour=category,label=category)) +
+    geom_line(size=linesize) +
+    geom_text_repel(size=4,data=subset(ps_dt_time,time==max(times)),
+                    aes(x=max(times)+0.1,y=psup), xlim=c(8,12)) +
+    scale_y_pSup("Proportion\nin\nsupernatant") + scale_time()
+  
+  if (errorbars) {
+    plot_temp = plot_temp + geom_pointrange()
+    plot_time = plot_time + geom_pointrange()
+  }
+  
+  return(list(plot_time=plot_time,plot_temp=plot_temp))
+  
+}
+
+plot_from_input = function(s, errorbars, idType) {
+  if (grepl('=',s,fixed=TRUE)) { 
+    # This is a named list
+    ids = splitstring(s)
+    print(paste("named categories:",ids))
+    fun = plotgenes_categories
+  } else {
+    ids = toupper(strsplit(gsub(" ", "", s, fixed = TRUE),",")[[1]])
+    print(paste("flat list:",ids))
+    fun = plotmygenes
+  }
+  fun(ids, errorbars=errorbars, idType=idType)
+}
+
 # Define server logic required to draw a plot
 shinyServer(function(input, output,session) {
     
@@ -98,8 +189,8 @@ shinyServer(function(input, output,session) {
     
     output$plot <- renderPlot({
         # time plot of 46C psup
-        ids <- toupper(strsplit(gsub(" ", "", input$ids, fixed = TRUE),",")[[1]])
-        plots <- plotmygenes(ids,errorbars=input$interval,idType=input$idType)
+        
+        plots = plot_from_input(input$ids, errorbars=input$interval,idType=input$idType)
         if (input$plotType == "time") {
             return(plots$plot_time)
         } else if (input$plotType == "temperature") {
